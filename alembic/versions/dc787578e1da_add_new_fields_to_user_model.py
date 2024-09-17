b8712c9216a4f9a58288e99294a9ed1fea9ad4dc
Kirill Dubasov <cergocergo41@gmail.com>
2024-09-17 18:17:58 +0300

    add migration file

---
+++ alembic/versions/dc787578e1da_add_new_fields_to_user_model.py
@@ -0,0 +1,26 @@
+"""Add new fields to User model
+
+Revision ID: dc787578e1da
+Revises: 0f325784201f
+Create Date: 2024-09-17 18:17:41.208498
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'dc787578e1da'
+down_revision: Union[str, None] = '0f325784201f'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
