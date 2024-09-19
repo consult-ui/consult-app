6c895202b95c0357b95d4e50d282255e6788c185
maxim.shnyagin <mshnyagin@evrone.com>
2024-09-19 17:23:45 +0300

    messages table

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ from sqladmin import Admin
 import app.routers.auth as auth
 import app.routers.organization as organization
 import app.routers.user as user
-from app.admin import AdminAuth, UserAdmin, RefreshSessionAdmin, OrganizationAdmin, ChatAdmin
+from app.admin import AdminAuth, UserAdmin, RefreshSessionAdmin, OrganizationAdmin, ChatAdmin, MessageAdmin
 from app.config import settings, STATIC_FOLDER
 from app.db import sessionmanager
 from app.exceptions import register_exception_handlers
@@ -74,3 +74,4 @@ admin.add_view(UserAdmin)
 admin.add_view(RefreshSessionAdmin)
 admin.add_view(OrganizationAdmin)
 admin.add_view(ChatAdmin)
+admin.add_view(MessageAdmin)
