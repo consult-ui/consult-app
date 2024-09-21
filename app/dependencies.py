e0e8dbbecc488061aa8cc85eedffa24ebe89b599
maxim.shnyagin <mshnyagin@evrone.com>
2024-09-21 14:08:20 +0300

    chat list

--- app/dependencies.py
+++ app/dependencies.py
@@ -53,7 +53,7 @@ async def get_active_user(id: CurrentUserIdDep, db_session: DBSessionDep) -> Use
 ActiveUserDep = Annotated[User, Depends(get_active_user)]
 
 
-async def get_organization_id(x_org_id: Annotated[str, Header()] = None) -> Optional[int]:
+async def get_organization_id(x_org_id: Annotated[str | None, Header()] = None) -> Optional[int]:
     if not x_org_id:
         return None
     return int(x_org_id)
