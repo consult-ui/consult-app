7973b124ac0a2373270ca200a42fe039e76d0eb2
maxim.shnyagin <mshnyagin@evrone.com>
2024-09-28 13:52:02 +0300

    delete file

--- app/service/openai.py
+++ app/service/openai.py
@@ -3,7 +3,3 @@ from openai import AsyncOpenAI
 from app.config import settings
 
 openai_client = AsyncOpenAI(api_key=settings.openai_api_key)
-
-openai_client.beta.assistants.create()
-
-openai_client.beta.assistants.retrieve()
