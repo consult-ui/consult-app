752b18dc769ffa8b2733c8e13047be7ad8ad25fd
maxim.shnyagin <mshnyagin@evrone.com>
2024-10-04 01:27:42 +0300

    msg

--- app/crud/messages.py
+++ app/crud/messages.py
@@ -8,6 +8,9 @@ from app.models.message import Message
 
 async def get_chat_messages(db_session: AsyncSession, chat_id: int) -> Sequence[Message]:
     rs = await db_session.execute(
-        select(Message).where(Message.chat_id == chat_id)
+        select(Message)
+        .where(Message.chat_id == chat_id)
+        .order_by(Message.created_at.desc())
+        .limit(100)
     )
     return rs.scalars().all()
