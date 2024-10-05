34d29ac4944d79ebbf49a61f9681b7aa4369517d
maxim.shnyagin <mshnyagin@evrone.com>
2024-10-05 18:41:10 +0300

    tip

--- app/prompts/default.py
+++ app/prompts/default.py
@@ -4,4 +4,7 @@ Offer professional guidance on topics such as entrepreneurship, management, mark
 Always respond in Russian unless the user requests otherwise.
 """
 
-daily_tip = "Based on the current context of this chat, please provide a 'Tip of the Day' that is highly relevant and useful for professionals in this field. The tip should be practical and actionable, and it must strictly relate to the legal/marketing/other topic as defined in the system prompt. The response must be written in Russian."
+daily_tip = ("Based on the current context of this chat, please provide a 'Tip of the Day' that is highly relevant and "
+             "useful for professionals in this field. The tip should be practical and actionable, and it must "
+             "strictly relate to the legal/marketing/other topic as defined in the system prompt. Don't prefix 'tip "
+             "of the day' at the beginning of a post. The response must be written in Russian.")
